4075b09115b5ba34ddba180ca73812a96188083e
Sathishkumar P <p.sathishkumar.mca@outlook.com>
2024-09-14 11:07:57 +0530

    More reliable and understandable/readable class names

--- Insurance-Management/src/test/java/com/InsuranceApplication/SpringBootInsuranceApplicationTests.java
+++ Insurance-Management/src/test/java/com/InsuranceApplication/SpringBootInsuranceApplicationTests.java
@@ -1,10 +1,10 @@
-package com.lic.Springbootinsurancemanagementplatform;
+package com.InsuranceApplication;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringBootInsuranceManagementPlatformApplicationTests {
+class SpringBootInsuranceApplicationTests {
 
 	@Test
 	void contextLoads() {
